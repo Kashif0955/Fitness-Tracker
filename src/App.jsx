126c0cbde5643d5fb8a3062e983c5a6d1f7dff90
unknown <Administrator@MJDT-0357.jinnah.edu>
2024-12-14 16:57:20 +0500

    frontend completed

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HeroSection from './Components/Hero'
+import AboutSection from './Pages/About'
+const App = () => {
+  return (
+    <>
+    
+      <BrowserRouter>
+      <HeroSection/>
+   
+        <Routes>
+          <Route to="/" element={<HeroSection/>}/>
+          <Route to="/about" element={<AboutSection/>}/>
+        </Routes>
+    
+
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
